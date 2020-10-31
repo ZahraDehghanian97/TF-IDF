a9e4ebbf491df2bec1f6ece4ae542d368aba3f90
zahra dehghanian <z.dehghanian@aut.ac.ir>
2020-10-31 09:24:05 +0330

    add TF-IDF for query

--- TF_IDF.py
+++ TF_IDF.py
@@ -2,18 +2,20 @@ from nltk.tokenize import word_tokenize
 import nltk
 import numpy as np
 import pandas as pd
+from scipy import spatial as spatial
 
 # this part defines path of data files
-corpusPath = 'Dataset\Retrieval.txt'
+corpusPath = 'Dataset\Corpus.txt'
 queryPath = 'Dataset\Query.txt'
 judgmentPath = 'Dataset\Judgement.txt'
 resultPath = 'Dataset\Result.txt'
+retrievalPath = 'Dataset\Retrieval.txt'
 
 
 # this func is used to parse the corpus file, it returns array of DID, Corpus, distinct words in all documents, and
 # count of words in each document
 
-def parseCorpus(path):
+def parseText(path):
     file = open(path, encoding='utf-8')
     lineNum = 0
     list = []
@@ -52,9 +54,8 @@ def parseCorpus(path):
     return list, doc_info, distinct, doc_length
 
 
-
-# this function is used to parse Querys file. given the name of tag it returns all the data within it
-def parseXML(path, tag):
+# this function is used to extract each tag. given the name of tag it returns all the data within it
+def extractTag(path, tag):
     begin_str = '<' + tag + '>'
     end_str = '</' + tag + '>'
     token = ""
@@ -63,9 +64,11 @@ def parseXML(path, tag):
     values = []
     for line in file:
         match = line.find(begin_str)
+        match2 = line.find(end_str)
         if match == 0:
             begin = 1
-        match2 = line.find(end_str)
+        elif begin == 1 and match2 == -1:
+            token += line
         if match2 == 0:
             begin = 0
             token = token.replace("\n", "")
@@ -73,86 +76,30 @@ def parseXML(path, tag):
             token.replace("،", " ")
             values.append(token)
             token = ""
-        elif begin == 1 and match == -1 and match2 == -1:
-            token += line
     return values
 
 
-# this function calculates term frequency and stores it array called TF_IDF_array
-def calculate_TF():
-    docID = 0
-    for doc in corpus:
-        words = word_tokenize(doc)
-        for word in words:
-            i, = np.where(distinct == word)
-            TF_IDF_array[i, docID] += 1
-        docID += 1
-    # normalization step
-    tmp = 0
-    for doc in DID:
-        TF_IDF_array[:, tmp] /= doc_length[tmp]
-        tmp += 1
-    return
+# this func combine arrays of str
+def combineArrays(title, description, narrative):
+    res = []
+    tmp = len(title)
+    for i in range(0, tmp):
+        str = title[i] + " "
+        str += description[i] + " "
+        str += narrative[i] + " "
+        res.append(str)
+    return res
 
 
-# this function calculates IDF for each word in corpus and stores it in array called IDF
-def calculate_IDF():
-    col = np.shape(TF_IDF_array)[0]
-    for term in range(0, col):
-        df = np.count_nonzero(TF_IDF_array[term, :])
-        if df != 0:
-            df = np.math.log(float(len(corpus)) / df)
-        IDF[term, 0] = df
-    return
 
-
-# this function calculates TF-IDF score and writes it in TF_IDF_array
-# multiplies IDF of each word with it's TF
-def calculate_TF_IDF():
-    cols = np.shape(TF_IDF_array)[1]
-    rows = np.shape(TF_IDF_array)[0]
-    for col in range(0, cols):
-        for row in range(0, rows):
-            TF_IDF_array[row, col] = TF_IDF_array[row, col] * (IDF[row])
-    return
-
-
-# this func returns norm of an array
-def norm(a):
-    return np.math.sqrt(np.dot(a, a))
-
-
-# this func calculates cosine similarity of two arrays
-def cosine_similarity(a, b):
-    if norm(a) > 0 and norm(b) > 0:
-        return np.dot(a, b) / (norm(a) * norm(b))
-    else:
-        return 0
-
-# this func returns the corresponding IDF array of given query terms.
-def query_vector(query_terms):
-    query_terms = set(query_terms)
-    n_terms, _ = TF_IDF_array.shape
-    query_vector = np.zeros(n_terms)
-    index = 0
-    for word in distinct:
-        if word in query_terms:
-            query_vector[index] = IDF[index, 0]
-        index += 1
-    return query_vector
-
-
-# this function returns the related documents to given query terms
-def Query(query_terms):
-    q = query_vector(query_terms)
-    n_terms, _ = TF_IDF_array.shape
-    results = []
-    index = 0
-    for doc in DID:
-        doc_vec = TF_IDF_array[:, index]
-        results.append((doc, cosine_similarity(q, doc_vec)))
-        index += 1
-    return sorted(results, key=lambda t: t[1], reverse=True)
+# this function is used to parse Query file
+def parseQuery(queryPath):
+    QID = extractTag(queryPath, 'QID')
+    title = extractTag(queryPath, 'title')
+    description = extractTag(queryPath, 'description')
+    narrative = extractTag(queryPath, 'narrative')
+    main_text_query = combineArrays(title, description, narrative)
+    return QID, main_text_query
 
 
 # this func reads judgments from given file
@@ -163,6 +110,67 @@ def parseJudgment(path):
         words = nltk.word_tokenize(line)
         judges.append(words)
     return judges
+
+
+# this function calculates term frequency and stores it array called TF_IDF_array
+def calculate_TF(type, normalize, text, TF_IDF_array):
+    docID = 0
+    for doc in text:
+        words = word_tokenize(doc)
+        for word in words:
+            i, = np.where(distinct == word)
+            TF_IDF_array[i, docID] += 1
+            if (type == 2) and (TF_IDF_array[i, docID] > 0):
+                TF_IDF_array[i, docID] = 1
+        docID += 1
+    if normalize == 1:
+        # normalization step
+        tmp = 0
+        for doc in DID:
+            TF_IDF_array[:, tmp] /= doc_length[tmp]
+            tmp += 1
+    return TF_IDF_array
+
+
+# this function calculates IDF for each word in corpus and stores it in array called IDF
+def calculate_IDF(TF_IDF_array, main_text):
+    col = np.shape(TF_IDF_array)[0]
+    for term in range(0, col):
+        df = np.count_nonzero(TF_IDF_array[term, :])
+        if df != 0:
+            df = np.math.log(float(len(main_text)) / df)
+        IDF[term, 0] = df
+    return
+
+
+# this function calculates TF-IDF score and writes it in TF_IDF_array
+# multiplies IDF of each word with it's TF
+def calculate_TF_IDF(TF_IDF_array):
+    cols = np.shape(TF_IDF_array)[1]
+    rows = np.shape(TF_IDF_array)[0]
+    for col in range(0, cols):
+        for row in range(0, rows):
+            TF_IDF_array[row, col] = TF_IDF_array[row, col] * (IDF[row])
+    return TF_IDF_array
+
+
+# this func returns the corresponding IDF array of given query terms.
+def query_vector(counter):
+    query_vector = TF_IDF_array_query[:, counter]
+    return query_vector
+
+
+# this function returns the related documents to given query terms
+def Query(counter):
+    q_vec = query_vector(counter)
+    n_terms, _ = TF_IDF_array.shape
+    results = []
+    index = 0
+    for doc in DID:
+        doc_vec = TF_IDF_array[:, index]
+        results.append((doc, spatial.distance.cosine(q_vec, doc_vec)))
+        index += 1
+    return sorted(results, key=lambda t: t[1], reverse=True)
 
 
 # this func evaluates given results based on gold data with precision@k measure
@@ -184,13 +192,24 @@ def getGold(QID):
     return goldData
 
 
+# this func writes results to given file
+def writeToFile(f, res):
+    f.write("[")
+    for ID, precision in res:
+        f.write("\n".join(["(%s , %s)" % (ID, precision)]))
+    f.write("]\n")
+    return
+
+
 # this function evaluates set of given queries with given parameters which are used as k in precision@k measure
 # and writes results to 'pathToWriteResults' file
-def evalquery(queries, precisions):
+def evalquery(precisions):
     query_ID = 0
     f = open(resultPath, 'a')
-    for query in queries:
-        res = Query(word_tokenize(query))
+    counter_query = 0
+    for query in main_text_query:
+        res = Query(counter_query)
+        counter_query += 1
         for precision in precisions:
             precisionAtK = evaluation(res, getGold(QID[query_ID]), precision)
             template = "precision @ %s  for QID %s  is: %s \n"
@@ -202,60 +221,34 @@ def evalquery(queries, precisions):
     return
 
 
-# this func writes results to given file
-def writeToFile(f, res):
-    f.write("[")
-    for ID, precision in res:
-        f.write("\n".join(["(%s , %s)" % (ID, precision)]))
-    f.write("]\n")
-    return
+# read files and parse them
+main_text, DID, distinct, doc_length = parseText(retrievalPath)
+main_text_corpus, DID_corpus, distinct_corpus, doc_length_corpus = parseText(corpusPath)
+QID, main_text_query = parseQuery(queryPath)
+judge = parseJudgment(judgmentPath)
+print("parsing file finished")
 
-
-# this func combine two arrays of str
-def combineTwoArrays(title, description):
-    res = []
-    tmp = len(title)
-    for i in range(0, tmp):
-        str = title[i] + " "
-        str += description[i] + " "
-        res.append(str)
-    return res
-
-
-# read corpus file and parse it
-corpus, DID, distinct, doc_length = parseCorpus(corpusPath)
-print("parse corpus file finished")
-# create a TF-IDF array and fill it using above functions
+# create a TF-IDF array for document and query
 TF_IDF_array = np.zeros([len(distinct), np.shape(DID)[0]])
+TF_array_corpus = np.zeros([len(distinct), np.shape(DID_corpus)[0]])
+TF_IDF_array_query = np.zeros([len(distinct), np.shape(QID)[0]])
 IDF = np.zeros([len(distinct), 1])
 print("built array to save result")
 
-calculate_TF()
+# calculate TF_IDF array for doc and query file
+type = 1  # 1 == count term frequency  2 == binary term frequency
+normalize = 0  # 0 == not normalize tf   1== normalize tf
+TF_IDF_array = calculate_TF(type, normalize, main_text, TF_IDF_array)
+TF_IDF_array_query = calculate_TF(type, normalize, main_text_query, TF_IDF_array_query)
 print("calculating TF finished")
-calculate_IDF()
+TF_array_corpus = calculate_TF(2, 0, main_text_corpus, TF_array_corpus)
+calculate_IDF(TF_array_corpus, main_text_corpus)
 print("calculating IDF finished")
-calculate_TF_IDF()
+TF_IDF_array = calculate_TF_IDF(TF_IDF_array)
+TF_IDF_array_query = calculate_TF_IDF(TF_IDF_array_query)
 print("calculating TF_IDF finished")
-print("calculation step for corpus file finished")
 
-# pd.DataFrame(TF_IDF_array).to_csv("data/TF_IDF_array")
-# TF_IDF_array = np.array(pd.read_csv("data/TF_IDF_array"))
-# print("read TF_IDF_array from saved file")
-
-# extract data from Query files
-QID = parseXML(queryPath, 'QID')
-title = parseXML(queryPath, 'title')
-description = parseXML(queryPath, 'description')
-titleAndDescription = combineTwoArrays(title, description)
-print("parse query file finished")
-
-# extract data from judgment data file
-judge = parseJudgment(judgmentPath)
-print("parse judgment file finished")
-
-# evaluate queries with their title, description, narrative using p@5 p@10 p@20
+# evaluate queries using p@5 p@10 p@20
 print("start evaluation")
 precision = [5, 10, 20]
-evalquery(title, precision)
-evalquery(description, precision)
-evalquery(titleAndDescription, precision)
+evalquery(precision)
