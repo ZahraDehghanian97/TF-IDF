dc4fbf5ef0d42cfd569e0b900e83446dff4dfc18
zahra dehghanian <z.dehghanian@aut.ac.ir>
2020-11-04 11:33:11 +0330

    change code to problem definition

--- TF_IDF.py
+++ TF_IDF.py
@@ -4,11 +4,11 @@ import numpy as np
 from scipy import spatial as spatial
 
 # this part defines path of data files
-corpusPath = 'Dataset\Corpus.txt'
+corpusPath = 'Dataset\Corpus2.txt'
 queryPath = 'Dataset\Query.txt'
 judgmentPath = 'Dataset\Judgement.txt'
 resultPath = 'Dataset\Result.txt'
-retrievalPath = 'Dataset\Retrieval.txt'
+retrievalPath = 'Dataset\Retrieval2.txt'
 
 
 # this func is used to parse the corpus file, it returns array of DID, Corpus, distinct words in all documents, and
@@ -110,116 +110,142 @@ def parseJudgment(path):
     return judges
 
 
+# this function calculates IDF for each word in corpus and stores it in array called IDF
+def calculate_IDF(distinct, main_text):
+    IDF1 = np.zeros([len(distinct)])
+    for i in range(len(distinct)):
+        temp = 0
+        for j in range(len(main_text)):
+            temp += main_text[j].count(distinct[i])
+        if temp != 0: IDF1[i] = np.math.log(float(len(main_text)) / temp)
+    return IDF1
+
+
 # this function calculates term frequency and stores it array called TF_IDF_array
 # type true count - type false binary
-def calculate_TF(type, normalize, text, array):
-    TF_IDF_array = np.array(array)
+def calculate_TF_IDF(text):
+    TF_IDF_array = np.zeros([len(distinct), len(text)])
     docID = 0
     for doc in text:
         words = (word_tokenize(doc))
         l = len(words)
-        if type:
-            for word in words:
-                i, = np.where(distinct == word)
-                TF_IDF_array[i, docID] += 1
-        else:
-            for word in words:
-                i, = np.where(distinct == word)
-                if TF_IDF_array[i, docID]==0 : TF_IDF_array[i, docID] = 1
-        if normalize: TF_IDF_array[:, docID] /= l
+        for word in words:
+            i, = np.where(distinct == word)
+            TF_IDF_array[i, docID] += IDF[i]
         docID += 1
     return TF_IDF_array
 
 
-# this function calculates IDF for each word in corpus and stores it in array called IDF
-def calculate_IDF(TF_IDF_array, main_text):
-    col = np.shape(TF_IDF_array)[0]
-    for term in range(0, col):
-        df = np.count_nonzero(TF_IDF_array[term, :])
-        if df != 0:
-            df = np.math.log(float(len(main_text)) / df)
-            IDF[term, 0] = df
-    return
+# this function changes a numerical matrix to a binary ones.
+def change_to_binary(array):
+    result = np.zeros([len(array), len(array[0])])
+    counter1 = 0
+    for i in array:
+        counter2 = 0
+        for j in i:
+            if j > 0: result[counter1, counter2] = IDF[counter2]
+            counter2 += 1
+        counter1 += 1
+    return result
 
 
-# this function calculates TF-IDF score and writes it in TF_IDF_array
-# multiplies IDF of each word with it's TF
-def calculate_TF_IDF(TF_IDF_array):
-    cols = np.shape(TF_IDF_array)[1]
-    rows = np.shape(TF_IDF_array)[0]
-    for col in range(0, cols):
-        for row in range(0, rows):
-            TF_IDF_array[row, col] = TF_IDF_array[row, col] * (IDF[row])
-    return TF_IDF_array
+# this func returns norm of an array
+def norm(a):
+    return np.math.sqrt(np.dot(a, a))
 
 
-# this func returns the corresponding IDF array of given query terms.
-def query_vector(counter):
-    query_vector = TF_IDF_array_query[:, counter]
-    return query_vector
+# this func calculates cosine distance of two arrays
+def cosine_distance(a, b):
+    if norm(a) > 0 and norm(b) > 0:
+        return np.dot(a, b) / (norm(a) * norm(b))
+    else:
+        return 0
+
+
+# this func calculates jaccard distance of two arrays
+def jaccard_distance(a, b):
+    intersection = 0.0
+    union = 0.0
+    for i in range(len(a)):
+        if a[i] > 0 and a[i] == b[i]: intersection += 1
+        if a[i] > 0 or b[i] > 0: union += 1
+    return intersection / union
 
 
 # this function returns the related documents to given query terms
-def Query(counter):
-    q_vec = query_vector(counter)
-    n_terms, _ = TF_IDF_array.shape
+def Query(type_distance, q_vec, array_doc):
+    # q_vec = array_query[:, counter]
     results = []
     index = 0
-    for doc in DID:
-        doc_vec = TF_IDF_array[:, index]
-        results.append((doc, spatial.distance.cosine(q_vec, doc_vec)))
+    for docID in DID:
+        doc_vec = array_doc[:, index]
+        if type_distance == "cosine":
+            results.append((docID, cosine_distance(q_vec, doc_vec)))
+        elif type_distance == "jaccard":
+            results.append((docID, jaccard_distance(q_vec, doc_vec)))
         index += 1
     return sorted(results, key=lambda t: t[1])
 
 
-# this func evaluates given results based on gold data with precision@k measure
-def evaluation(result, gold, k):
-    tmp = 0
-    if k > len(result): k = len(result)
-    for i in range(0, k):
-        if result[i][0] in gold:
-            tmp += 1
-        print(str(result[i][0]))
-    return (tmp / k)
+# print 15 similar doc
+def part_a(array_query, array_doc):
+    distance = ["cosine", "jaccard"]
+    for d in distance:
+        index = 0
+        for q_vec in array_query:
+            print("15 doc similar to " + str(QID[index]) + "with " + str(d) + " distance is :")
+            print(Query(d, q_vec, array_doc)[:15])
+            index += 1
 
 
-# this func returns the gold data related to given QID
-def getGold(QID):
-    row = np.shape(judge)[0]
-    goldData = []
-    for i in range(0, row):
-        if QID == judge[i][0]:
-            goldData.append(judge[i][1])
-    return goldData
-
-
-# this func writes results to given file
-def writeToFile(f, res):
-    f.write("[")
-    for ID, precision in res:
-        f.write("\n".join(["(%s , %s)" % (ID, precision)]))
-    f.write("]\n")
-    return
-
-
-# this function evaluates set of given queries with given parameters which are used as k in precision@k measure
-# and writes results to 'pathToWriteResults' file
-def evalquery(precisions):
-    query_ID = 0
-    f = open(resultPath, 'a')
-    counter_query = 0
-    for query in main_text_query:
-        res = Query(counter_query)
-        counter_query += 1
-        for precision in precisions:
-            precisionAtK = evaluation(res, getGold(QID[query_ID]), precision)
-            template = "precision @ %s  for QID %s  is: %s \n"
-            f.write("\n")
-            print(template % (precision, QID[query_ID], precisionAtK))
-            f.write(template % (precision, QID[query_ID], precisionAtK))
-            writeToFile(f, res[:precision])
-        query_ID += 1
-    return
+# # this func evaluates given results based on gold data with precision@k measure
+# def evaluation(result, gold, k):
+#     tmp = 0
+#     if k > len(result): k = len(result)
+#     for i in range(0, k):
+#         if result[i][0] in gold:
+#             tmp += 1
+#         print(str(result[i][0]))
+#     return (tmp / k)
+#
+#
+# # this func returns the gold data related to given QID
+# def getGold(QID):
+#     row = np.shape(judge)[0]
+#     goldData = []
+#     for i in range(0, row):
+#         if QID == judge[i][0]:
+#             goldData.append(judge[i][1])
+#     return goldData
+#
+#
+# # this func writes results to given file
+# def writeToFile(f, res):
+#     f.write("[")
+#     for ID, precision in res:
+#         f.write("\n".join(["(%s , %s)" % (ID, precision)]))
+#     f.write("]\n")
+#     return
+#
+#
+# # this function evaluates set of given queries with given parameters which are used as k in precision@k measure
+# # and writes results to 'pathToWriteResults' file
+# def evalquery(type_distance, precisions):
+#     query_ID = 0
+#     f = open(resultPath, 'a')
+#     counter_query = 0
+#     for query in main_text_query:
+#         res = Query(type_distance, query, TF_IDF_array)
+#         counter_query += 1
+#         for precision in precisions:
+#             precisionAtK = evaluation(res, getGold(QID[query_ID]), precision)
+#             template = "precision @ %s  for QID %s  is: %s \n"
+#             f.write("\n")
+#             print(template % (precision, QID[query_ID], precisionAtK))
+#             f.write(template % (precision, QID[query_ID], precisionAtK))
+#             writeToFile(f, res[:precision])
+#         query_ID += 1
+#     return
 
 
 # read files and parse them
@@ -238,27 +264,28 @@ print("avg length of docs: " + str(sum(doc_length) / len(doc_length)))
 print("doc with max length: " + str(DID[doc_length.index(max(doc_length))]))
 print("doc with min length: " + str(DID[doc_length.index(min(doc_length))]))
 
-# create a TF-IDF array for document and query
-TF_IDF_array = np.zeros([len(distinct), np.shape(DID)[0]])
-TF_array_corpus = np.zeros([len(distinct), np.shape(DID_corpus)[0]])
-TF_IDF_array_query = np.zeros([len(distinct), np.shape(QID)[0]])
-IDF = np.zeros([len(distinct), 1])
-print("built array to save result")
+# # calculate TF_IDF array for doc and query file
+# IDF = calculate_IDF(distinct, main_text_corpus)
+# print("calculating IDF finished")
+# TF_IDF_array = calculate_TF_IDF( main_text)
+# TF_IDF_array_query = calculate_TF_IDF( title_query)
+# print("calculating TF-IDF finished")
 
-# calculate TF_IDF array for doc and query file
-type = True  # True == count term frequency  False== binary term frequency
-normalize = True  # False == not normalize tf   True== normalize tf
-TF_IDF_array = calculate_TF(type, normalize, main_text, TF_IDF_array)
-TF_IDF_array_query = calculate_TF(type, normalize, main_text_query, TF_IDF_array_query)
-print("calculating TF finished")
-TF_array_corpus = calculate_TF(type, normalize, main_text_corpus, TF_array_corpus)
-calculate_IDF(TF_array_corpus, main_text_corpus)
+# # evaluate queries using p@5 p@10 p@20
+# print("start evaluation")
+# precision = [5, 10]
+# evalquery(precision)
+
+# part a
+print("compute 15 similar doc with cosine distance")
+IDF = calculate_IDF(distinct, main_text_corpus)
 print("calculating IDF finished")
-TF_IDF_array = calculate_TF_IDF(TF_IDF_array)
-TF_IDF_array_query = calculate_TF_IDF(TF_IDF_array_query)
-print("calculating TF_IDF finished")
-
-# evaluate queries using p@5 p@10 p@20
-print("start evaluation")
-precision = [5, 10]
-evalquery(precision)
+TF_IDF_array = calculate_TF_IDF(main_text)
+TF_IDF_array_query = calculate_TF_IDF(title_query)
+TF_IDF_array_binary = change_to_binary(TF_IDF_array)
+TF_IDF_array_query_binary = change_to_binary(TF_IDF_array_query)
+print("calculating TF-IDF finished")
+print("********numeric part********")
+part_a(TF_IDF_array_query, TF_IDF_array)
+print("********binary part********")
+part_a(TF_IDF_array_query_binary, TF_IDF_array_binary)
